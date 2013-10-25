32ebc6143e123815dd0926167f04a670b7918eb3
rpelyush <rpelyush@gmail.com>
2013-10-25 14:11:15 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demandi::Application.config.secret_key_base = '584094fcc7937885415273c6a6b83eed768cfa40cac280f2ab0d37ac03246aea86c9dc9abbcb4a04d3716fd665cd37cebd3ba0b533557490fc2730d10d8b5f2a'
